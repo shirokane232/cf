3385987fc584d22ff7e4036a65eb4505cc1322b1
shirokane232 <shirokane232@gmail.com>
2025-08-31 15:57:34 +0800

    threads basic

--- server/api/threads/auth/login.ts
+++ server/api/threads/auth/login.ts
@@ -10,9 +10,7 @@ export default defineEventHandler(async (event) => {
     return { error: 'Client ID or Redirect URI not configured.' };
   }
 
-  // 產生 Threads 授權 URL
-  // `scope` 參數指定您需要的權限，這裡以 `threads_basic` 和 `threads_content_publish` 為例
-  const authUrl = `https://graph.threads.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=threads_basic,threads_content_publish&response_type=code`;
+  const authUrl = `https://graph.threads.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=threads_basic&response_type=code`;
 
   // 執行重新導向到 Threads 授權頁面
   return sendRedirect(event, authUrl);
