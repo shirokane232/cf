4a4b06f2514f9631f767765e5a3e0c1ff07b526e
shirokane232 <shirokane232@gmail.com>
2025-08-31 12:22:55 +0800

    /version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 const commitHash = require('child_process').execSync('git rev-parse HEAD').toString().trim();
+const commitTime = require('child_process').execSync('git log -1 --format=%cI').toString().trim();
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
@@ -9,6 +11,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       commitHash: commitHash,
+      commitTime: commitTime,
     }
   }
 })
--- server/api/version.ts
+++ server/api/version.ts
@@ -2,5 +2,6 @@ export default defineEventHandler(async (event) => {
   const runtimeConfig = useRuntimeConfig();
   return {
     version: runtimeConfig.public.commitHash,
+    updatedAt: runtimeConfig.public.commitTime,
   }
 })
